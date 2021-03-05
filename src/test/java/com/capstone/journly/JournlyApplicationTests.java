a763bdde92161236219275ecc57e66b584734e29
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-03-05 16:07:07 -0600

    initial commit

---
+++ src/test/java/com/capstone/journly/JournlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.journly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
