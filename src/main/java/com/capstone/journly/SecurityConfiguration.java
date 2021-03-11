c932d726e1c53cafe09ae4fa7fd72347a69cd339
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-03-11 11:29:03 -0600

    add: header and update: SecurityConfiguration file

--- src/main/java/com/capstone/journly/SecurityConfiguration.java
+++ src/main/java/com/capstone/journly/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.capstone.journly;
 
-import com.capstone.journly.UserDetailsLoader;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
