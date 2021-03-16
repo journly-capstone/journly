d941d0a351b5b8d81eaf9b14c467e7827f952b1a
kcollier10 <kristencollier10@gmail.com>
2021-03-16 09:21:58 -0500

    feat: adding entry create methods to main

--- src/main/java/com/capstone/journly/controllers/GratitudeEntryController.java
+++ src/main/java/com/capstone/journly/controllers/GratitudeEntryController.java
@@ -1,11 +1,15 @@
 package com.capstone.journly.controllers;
 
 import com.capstone.journly.models.GratitudeEntry;
+import com.capstone.journly.models.User;
 import com.capstone.journly.repositories.GratitudeEntryRepository;
 import com.capstone.journly.repositories.UserRepository;
+import com.capstone.journly.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -15,12 +19,13 @@ public class GratitudeEntryController {
     private final GratitudeEntryRepository gratitudeEntryDao;
     private final UserRepository usersDao;
 //    private final EmailService emailService;
-//    private final UserService userService;
+    private final UserService userService;
 
 
-    public GratitudeEntryController(GratitudeEntryRepository gratitudeEntryDao, GratitudeEntryRepository gratitudeEntryDao1, UserRepository usersDao) {
+    public GratitudeEntryController(GratitudeEntryRepository gratitudeEntryDao, GratitudeEntryRepository gratitudeEntryDao1, UserRepository usersDao, UserService userService) {
         this.gratitudeEntryDao = gratitudeEntryDao1;
         this.usersDao = usersDao;
+        this.userService = userService;
     }
 
     @GetMapping("/gratitude-board")
@@ -29,5 +34,22 @@ public class GratitudeEntryController {
         model.addAttribute("entries", entries);
 
         return "gratitude-entries";
+    }
+
+    // edit URL in below method later?
+    @GetMapping(path ="/gratitude-board/create")
+    public String createGratitudeEntryGET(Model model) {
+        model.addAttribute("newEntry", new GratitudeEntry());
+        return "create-gratitude-entry";
+    }
+
+    @PostMapping(path = "/gratitude-board/create")
+    public String createGratitudeEntryPOST (@ModelAttribute GratitudeEntry entry) {
+        User user = userService.getLoggedInUser();
+        entry.setUser(user);
+        GratitudeEntry savedEntry = gratitudeEntryDao.save(entry);
+
+        // map to single post view or profile view, etc
+        return "redirect:/gratitude-board";
     }
 }
