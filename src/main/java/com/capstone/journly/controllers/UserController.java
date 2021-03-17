33d7f92d70e537fcec4e6bcde601a7c9d17720b2
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-03-17 02:34:12 -0500

    feat: allow users to confirm passwords upon sign up and allow users to update passwords (in profile settings)

--- src/main/java/com/capstone/journly/controllers/UserController.java
+++ src/main/java/com/capstone/journly/controllers/UserController.java
@@ -6,9 +6,10 @@ import com.capstone.journly.services.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -35,9 +36,9 @@ public class UserController {
         return "users/profile-settings";
     }
 
-    //Allow User to Update Profile Settings
-    @PostMapping("/profile-settings")
-    public String updateProfileSettings(@ModelAttribute User user, Model model) {
+    //Allow Users to Update Profile Information
+    @PostMapping("/profile-settings/update-profile-information")
+    public String updateProfileInformation(@ModelAttribute User user, Model model) {
         User sessionUser = userService.getLoggedInUser();
         User updatedUser = userDao.getOne(sessionUser.getId());
 
@@ -45,6 +46,84 @@ public class UserController {
         user.setPassword(updatedUser.getPassword());
         userDao.save(user);
 
-        return "redirect:/profile-settings";
+//        return "redirect:/profile-settings";
+        return "redirect:/profile-settings/#account-information";
     }
+
+
+//    //Show Change Password View to User
+//    @GetMapping("profile-settings/change-password")
+//    public String showChangePassword(Model model) {
+//        model.addAttribute("title", "Profile Settings - Change Password");
+//
+//        User sessionUser = userService.getLoggedInUser();
+//        User user = userDao.getOne(sessionUser.getId());
+//        model.addAttribute("user", user);
+//
+//        return "users/change-password";
+//    }
+
+
+    //Allow Users to Update Password
+    @PostMapping("/profile-settings/change-password")
+    public String changePassword(@RequestParam(name="password")String password, @RequestParam(name="confirm")String confirm, @RequestParam(name="id")Long id, Model model){
+        User user = userDao.getOne(id);
+
+        if (password.equals(confirm)){
+            String hash = encoder.encode(password);
+            user.setPassword(hash);
+            userDao.save(user);
+            return "login";
+        }
+//        return showChangePassword(model);
+
+        return "users/change-password";
+    }
+
+//    //Allow Users to Update Password
+//    @PostMapping("/profile-settings/change-password")
+//    public String changePassword(Model model, @Valid @ModelAttribute User user, Errors validation, @RequestParam(name = "confirm") String confirm, @RequestParam(name = "password")String password){
+//
+//        model.addAttribute("title", "Profile Settings - Change Password");
+////        model.addAttribute("user", user);
+//
+//        String hash = encoder.encode(user.getPassword());
+//
+//        if(!user.getPassword().equals(confirm)){
+//            validation.rejectValue(
+//                    "password",
+//                    "user.password",
+//                    "Passwords do not match"
+//            );
+//        }
+//
+//        if(validation.hasErrors()) {
+//            model.addAttribute("errors", validation);
+//            model.addAttribute("user", user);
+//
+//            return "users/profile-settings";
+//            //takes me straight to the profile-settings page (with account information tab open) although the url is as follows: profile-settings/change-password#change-password
+//            //but, does save changes on #change-password tab. just not the other tabs.
+//        }
+//
+//        if (password.equals(confirm)){
+//            user.setId(user.getId());
+//            user.setPassword(hash);
+//            userDao.save(user);
+//
+//            return "redirect:/login";
+//        }
+//
+////        if (password.equals(confirm)){
+////            user.setId(user.getId());
+////            String hash = encoder.encode(password);
+////            user.setPassword(hash);
+////            userDao.save(user);
+////
+////            return "redirect:/login";
+////        }
+//
+//        return "redirect:/profile-settings";
+//    }
+
 }
