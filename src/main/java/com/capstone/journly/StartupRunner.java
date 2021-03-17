af2a1994ecd9d1d4a8b3b1eca0512c0e51d6d2b3
kcollier10 <kristencollier10@gmail.com>
2021-03-16 19:14:30 -0500

    feat: GratitudeEntryController (ability for user to see a random prompt on create-gratitude-entries view), GratitudeEntry model (unused import but doing a little research on it), PromptRepository (randomized prompts and made a method that can be accessed), StartupRunner (added user on startup, having issue with the prompt field saying null so I need to work through that, **IMPORTANT --> this code is commented out due to prompt id access issue, feel free to ask me anything about it/play with the code on your end! <-- **), default.jpeg (just an image from pexels I'm testing with), create-gratitude-entry.html (small th:text addition to link GratitudeEntryController's @GetMapping(/gratitude-board/create) request to the random prompt method in the PromptRepository)

--- src/main/java/com/capstone/journly/StartupRunner.java
+++ src/main/java/com/capstone/journly/StartupRunner.java
@@ -1,3 +1,56 @@
 package com.capstone.journly;
-public class StartupRunner {
+
+
+
+import com.capstone.journly.models.GratitudeEntry;
+import com.capstone.journly.models.User;
+import com.capstone.journly.repositories.GratitudeEntryRepository;
+import com.capstone.journly.repositories.PromptRepository;
+import com.capstone.journly.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class StartupRunner implements CommandLineRunner {
+
+    private final UserRepository userDao;
+    private final GratitudeEntryRepository gratitudeEntryDao;
+    private final PasswordEncoder encoder;
+    private final PromptRepository promptDao;
+
+    public StartupRunner(UserRepository userDao, GratitudeEntryRepository gratitudeEntryDao, PasswordEncoder encoder, PromptRepository promptDao) {
+        this.userDao = userDao;
+        this.gratitudeEntryDao = gratitudeEntryDao;
+        this.encoder = encoder;
+        this.promptDao = promptDao;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (userDao.count() != 0) {
+            return;
+        }
+        User user = new User();
+        user.setUsername("cody");
+        user.setEmail("cody@codeup.com");
+        user.setPassword(encoder.encode("codeuprocks"));
+        userDao.save(user);
+
+        GratitudeEntry gratitudeEntry = new GratitudeEntry();
+        gratitudeEntry.setUser(user);
+        gratitudeEntry.setCreatedAt(new Date(System.currentTimeMillis()));
+        gratitudeEntry.setPrompt(promptDao.findRandomPrompt());
+        gratitudeEntry.setBody("lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ");
+        gratitudeEntry.setImgFilePath("/Users/kristencollier/IdeaProjects/journly/src/main/resources/static/uploads/default.jpeg");
+        gratitudeEntry.setIsPublic(true);
+
+        gratitudeEntryDao.save(gratitudeEntry);
+
+
+
+    }
 }
---
+++ src/main/java/com/capstone/journly/repositories/PromptRepository.java
@@ -0,0 +1,10 @@
+package com.capstone.journly.repositories;
+
+import com.capstone.journly.models.Prompt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PromptRepository extends JpaRepository<Prompt, Long> {
+    @Query(value = "SELECT * FROM prompts ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    public Prompt findRandomPrompt();
+}
