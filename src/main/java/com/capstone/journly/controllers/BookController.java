3fd50a28c9bece6ee6d4e9d1fc6e0eec15a8aef9
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-03-18 15:57:47 -0500

    fixed delete functionality

--- src/main/java/com/capstone/journly/controllers/BookController.java
+++ src/main/java/com/capstone/journly/controllers/BookController.java
@@ -1,5 +1,4 @@
 package com.capstone.journly.controllers;
-
 import com.capstone.journly.models.Book;
 import com.capstone.journly.models.Bookshelf;
 import com.capstone.journly.models.User;
@@ -13,21 +12,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.model.IModel;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class BookController {
-
     private final GratitudeEntryRepository gratitudeEntryDao;
-//    private final EmailService emailService;
+    //    private final EmailService emailService;
     private final UserService userService;
     private final BookRepository bookDao;
     private final BookshelfRepository bookshelfDao;
     private final UserRepository userDao;
-
     public BookController(GratitudeEntryRepository gratitudeEntryDao, BookRepository bookDao, UserService userService, BookshelfRepository bookshelfDao, UserRepository userDao) {
         this.gratitudeEntryDao = gratitudeEntryDao;
         this.userService = userService;
@@ -35,33 +31,30 @@ public class BookController {
         this.bookshelfDao = bookshelfDao;
         this.userDao = userDao;
     }
-
-//    public BookController(GratitudeEntryRepository gratitudeEntryDao,BookRepository bookDao, EmailService emailService, UserService userService) {
+    //    public BookController(GratitudeEntryRepository gratitudeEntryDao,BookRepository bookDao, EmailService emailService, UserService userService) {
 //        this.gratitudeEntryDao = gratitudeEntryDao;
 //        this.emailService = emailService;
 //        this.userService = userService;
 //        this.bookDao = bookDao;
 //    }
-
+//    Line 49 can possibly be removed*****************************
     @GetMapping("/books")
     public String postForm(Model model){
         model.addAttribute("book", new Book());
         return "books";
     }
-
-    @PostMapping("/books")
-    public String createPost(@ModelAttribute Book book) {
-        // Will throw if no users in the db!
-        // In the future, we will get the logged in user
-        User user = userService.getLoggedInUser();
-
-
-        Book savedBook = bookDao.save(book);
-
-
-        return "redirect:/books";
-    }
-
+    //    @PostMapping("/books")
+//    public String createPost(@ModelAttribute Book book) {
+//        // Will throw if no users in the db!
+//        // In the future, we will get the logged in user
+//        User user = userService.getLoggedInUser();
+//
+//
+//        Book savedBook = bookDao.save(book);
+//
+//
+//        return "redirect:/books";
+//    }
     @GetMapping("/bookshelf")
     public String getBookshelf(Model model){
         model.addAttribute("title", "My Bookshelf");
@@ -69,17 +62,14 @@ public class BookController {
         Bookshelf bookshelf = bookshelfDao.findByUser(user);
         List<Book> books = bookshelf.getBooks();
         model.addAttribute("books", books);
-
         return "bookshelf";
     }
-
     @PostMapping("/addbook")
     public String addBook(
             @RequestParam("bookThumbnail")String bookThumbnail,
             @RequestParam("id")String id,
             @RequestParam("author")String author,
             @RequestParam("title")String title){
-
         System.out.println(id);
         User user = userService.getLoggedInUser();
         Bookshelf bookshelf = bookshelfDao.findByUser(user);
@@ -107,9 +97,8 @@ public class BookController {
         Bookshelf updatedBookshelf = bookshelfDao.findByUser(user);
         List<Book> books = updatedBookshelf.getBooks();
         model.addAttribute("books", books);
-        return "partials/updateBookshelf :: #updatedBooks";
+        return "partials/updateBookshelf :: updatedBooks";
     }
-
 //    @GetMapping("/loadbookshelf")
 //    public String loadBookshelf(Model model){
 //        User user = userService.getLoggedInUser();
@@ -118,7 +107,4 @@ public class BookController {
 //        model.addAttribute("books", books);
 //        return "bookshelf";
 //    }
-
-
-
 }
