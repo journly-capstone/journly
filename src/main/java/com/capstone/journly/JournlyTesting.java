f0b0d5376917958372ee9f3585359402b2a53229
kcollier10 <kristencollier10@gmail.com>
2021-03-19 10:06:21 -0500

    feat: new files added

---
+++ src/main/java/com/capstone/journly/JournlyTesting.java
@@ -0,0 +1,13 @@
+package com.capstone.journly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournlyTesting {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
