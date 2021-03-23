edb6fdc432cc553d6db88d456eb7e8bd05a19082
kcollier10 <kristencollier10@gmail.com>
2021-03-23 10:41:29 -0500

    feat: update files reupdated

--- src/main/java/com/capstone/journly/controllers/GratitudeEntryController.java
+++ src/main/java/com/capstone/journly/controllers/GratitudeEntryController.java
@@ -1,5 +1,4 @@
 package com.capstone.journly.controllers;
-
 import com.capstone.journly.models.*;
 import com.capstone.journly.repositories.GratitudeEntryRepository;
 import com.capstone.journly.repositories.LikeRepository;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -19,16 +17,13 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-
 @Controller
 public class GratitudeEntryController {
-
     private final GratitudeEntryRepository gratitudeEntryDao;
     private final UserRepository userDao;
     private final UserService userService;
     private final PromptRepository promptDao;
     private final LikeRepository likeDao;
-
     public GratitudeEntryController(GratitudeEntryRepository gratitudeEntryDao, UserRepository userDao, UserService userService, PromptRepository promptDao, LikeRepository likeDao) {
         this.gratitudeEntryDao = gratitudeEntryDao;
         this.userDao = userDao;
@@ -36,19 +31,14 @@ public class GratitudeEntryController {
         this.promptDao = promptDao;
         this.likeDao = likeDao;
     }
-
     @Value("${file-upload-path}")
     private String uploadPath;
-
-
     @GetMapping("/gratitude-board")
     public String gratitudeBoard(Model model) {
         List<GratitudeEntry> entries = gratitudeEntryDao.findAll();
         model.addAttribute("entries", entries);
-
         return "gratitudes/gratitude-board";
     }
-
     @GetMapping("/gratitude-board/{id}")
     public String singleEntryViewMore (Model model, @PathVariable long id) {
         GratitudeEntry singleEntry = gratitudeEntryDao.getOne(id);
@@ -56,11 +46,10 @@ public class GratitudeEntryController {
         List<Like> likes = likeDao.findByGratitudeEntry(singleEntry);
         model.addAttribute("numOfLikes", likes.size());
         User user = userService.getLoggedInUser();
-        List<Like> hasLiked = likeDao.findAllByGratitudeEntryAndUser(singleEntry, user);
+        List<Like> hasLiked = likeDao.findByGratitudeEntryAndUser(singleEntry, user);
         model.addAttribute("hasLiked", hasLiked.size()>0);
         return "gratitudes/individual-gratitude-entry";
     }
-
     @GetMapping(path ="/gratitude-board/create")
     public String createGratitudeEntryGET(Model model) {
         model.addAttribute("gratitudeEntry", new GratitudeEntry());
@@ -68,16 +57,13 @@ public class GratitudeEntryController {
         model.addAttribute("prompt", prompt);
         return "gratitudes/create-gratitude-entry";
     }
-
-
     @PostMapping(path = "/gratitude-board/create")
     public String newGratitudeEntry(Model model,
-                         @RequestParam(name = "image") MultipartFile image,
-                         @RequestParam(name = "isPublic", defaultValue = "false") boolean isPublic,
-                         // ********************** //
-                         @RequestParam(name = "promptId") long promptId,
-                         @ModelAttribute GratitudeEntry gratitudeEntry) {
-
+                                    @RequestParam(name = "image") MultipartFile image,
+                                    @RequestParam(name = "isPublic", defaultValue = "false") boolean isPublic,
+                                    // ********************** //
+                                    @RequestParam(name = "promptId") long promptId,
+                                    @ModelAttribute GratitudeEntry gratitudeEntry) {
         if (image != null) {
             String filename = image.getOriginalFilename();
             String filepath = Paths.get(uploadPath, filename).toString();
@@ -93,7 +79,6 @@ public class GratitudeEntryController {
         } else {
             gratitudeEntry.setImgFilePath("/uploads/default.png");
         }
-
         gratitudeEntry.setUser(userService.getLoggedInUser());
         gratitudeEntry.setIsPublic(isPublic);
         gratitudeEntry.setPrompt(promptDao.getOne(promptId));
@@ -103,10 +88,8 @@ public class GratitudeEntryController {
 //        model.addAttribute("updatedDate", updatedDate);
         gratitudeEntry.setCreatedAt(new Date());
         gratitudeEntryDao.save(gratitudeEntry);
-
         return "redirect:/gratitude-board";
     }
-
     @PostMapping("/like_gratitude_entry")
     public String likeEntry(@RequestParam("gratitudeId")long gratitudeId){
         System.out.println("***************************");
@@ -120,26 +103,11 @@ public class GratitudeEntryController {
         likeDao.save(like);
         return"gratitudes/gratitude-board";
     }
-    @PostMapping("/unlike_gratitude_entry")
-    public String unlikeEntry(@RequestParam("gratitudeId")long gratitudeId,
-                              Model model){
-        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-        System.out.println(gratitudeId);
-        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-        User user = userService.getLoggedInUser();
-        GratitudeEntry gratitudeEntry = gratitudeEntryDao.getOne(gratitudeId);
-        Like like = likeDao.findByGratitudeEntryAndUser(gratitudeEntry, user);
-        likeDao.delete(like);
-        return"gratitudes/gratitude-board";
-    }
-
     @PostMapping("/dashboard/{id}/delete/")
     public String deleteGratitudeEntry(@RequestParam(name = "entryId") long entryId, Model model) {
         gratitudeEntryDao.deleteById(entryId);
-
         return "redirect:/dashboard";
     }
-
     @GetMapping("/dashboard/{id}/update")
     public String updateEntryGET(@PathVariable(name="id") long id, Model model) {
         User current = userDao.getOne(userService.getLoggedInUser().getId());
@@ -147,16 +115,13 @@ public class GratitudeEntryController {
         model.addAttribute("gratitudeEntry", gratitudeEntry);
         return "gratitudes/update-gratitude-entry";
     }
-
     @PostMapping("/dashboard/{id}/update")
     public String updateEntryPOST(
             @PathVariable(name = "id") long id,
             @RequestParam(name = "image") MultipartFile image,
             @RequestParam(name = "isPublic", defaultValue = "false") boolean isPublic,
             @ModelAttribute GratitudeEntry originalEntry) {
-
         GratitudeEntry updatedEntry = gratitudeEntryDao.getOne(id);
-
         if (image != null) {
             String filename = image.getOriginalFilename();
             String filepath = Paths.get(uploadPath, filename).toString();
@@ -170,9 +135,7 @@ public class GratitudeEntryController {
         }
         updatedEntry.setUser(userService.getLoggedInUser());
         updatedEntry.setIsPublic(isPublic);
-
         gratitudeEntryDao.save(updatedEntry);
-
         return "redirect:/dashboard";
     }
 }
