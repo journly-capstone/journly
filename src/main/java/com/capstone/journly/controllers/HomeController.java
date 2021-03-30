408d7e93a161162cff34d77775d4853c6746185d
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-03-30 15:52:18 -0500

    added visible likes to dashboard w style + added quote on about us page

--- src/main/java/com/capstone/journly/controllers/HomeController.java
+++ src/main/java/com/capstone/journly/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package com.capstone.journly.controllers;
 import com.capstone.journly.models.GratitudeEntry;
+import com.capstone.journly.models.Like;
 import com.capstone.journly.models.User;
 import com.capstone.journly.repositories.GratitudeEntryRepository;
+import com.capstone.journly.repositories.LikeRepository;
 import com.capstone.journly.repositories.QuoteRepository;
 import com.capstone.journly.repositories.UserRepository;
 import com.capstone.journly.services.UserService;
@@ -9,18 +11,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Controller
 public class HomeController {
     private final QuoteRepository quoteDao;
     private final UserRepository userDao;
     private final UserService userService;
     private final GratitudeEntryRepository gratitudeEntryDao;
+    private final LikeRepository likeDao;
 
-    public HomeController(QuoteRepository quoteDao, UserRepository userDao, UserService userService, GratitudeEntryRepository gratitudeEntryDao) {
+    public HomeController(QuoteRepository quoteDao, UserRepository userDao, UserService userService, GratitudeEntryRepository gratitudeEntryDao, LikeRepository likeDao) {
         this.quoteDao = quoteDao;
         this.userDao = userDao;
         this.userService = userService;
         this.gratitudeEntryDao = gratitudeEntryDao;
+        this.likeDao = likeDao;
     }
 //    private final PasswordEncoder encoder;
 //    public HomeController(UserRepository userDao, PasswordEncoder encoder) {
@@ -51,13 +58,26 @@ public class HomeController {
 
     @GetMapping("/dashboard")
     public String dashboard(Model model){
+
+        List<GratitudeEntry> entries = gratitudeEntryDao.findAll();
+        HashMap<Long, Integer> numOfLikes = new HashMap<Long, Integer>();
         User user = userService.getLoggedInUser();
         User currentUser = userDao.getOne(user.getId());
+
+        for (GratitudeEntry entry : entries) {
+
+            List<Like> likes = likeDao.findByGratitudeEntry(entry);
+            numOfLikes.put(entry.getId(), likes.size());
+
+        }
+
+        model.addAttribute("numOfLikes", numOfLikes);
         model.addAttribute("user", currentUser);
         model.addAttribute("title", "Dashboard");
         model.addAttribute("quotes", quoteDao.findRandomQuote());
         model.addAttribute("userEntries", currentUser.getUserGratitudeEntries());
         return "users/dashboard";
+
     }
 
 
