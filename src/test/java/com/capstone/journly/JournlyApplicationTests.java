81f7c4a2760a441b4c75f61eec0ab9d7b85a3237
kcollier10 <kristencollier10@gmail.com>
2021-04-05 22:12:22 -0500

    feat: working on adding testing to application

--- src/test/java/com/capstone/journly/JournlyApplicationTests.java
+++ src/test/java/com/capstone/journly/JournlyApplicationTests.java
@@ -1,7 +1,10 @@
 package com.capstone.journly;
 
+import com.capstone.journly.models.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.Test;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class JournlyApplicationTests {
@@ -9,5 +12,12 @@ class JournlyApplicationTests {
     @Test
     void contextLoads() {
     }
+
+//    @Test
+//    public void testCreateUser () {
+//        User testUser = new User(1L, "user");
+//    }
+
+
 
 }
